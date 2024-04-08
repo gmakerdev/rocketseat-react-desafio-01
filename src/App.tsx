2106541640cac97fffc6f3124378a4568ad21ba1
Gerson Lima <gerson.silva.lima@gmail.com>
2024-04-07 21:15:47 -0300

    setup home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.module.css'
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
 
   return (
       <div>
-        Setup Projeto Rocketseta - ReactJS - Desafio 01
+        <header className={styles.header}>
+          Teste
+        </header>
+        <div>
+
+        </div>
       </div>
   )
 }
